2c66ed4672e29e3ef85d8596ef4b74a2718ba964
scinloop <1151127150@qq.com>
2024-06-29 15:35:23 +0800

    首次

---
+++ graph.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(3,2),facecolor="green")
+plt.subplot(2,2,1)
+plt.subplot(2,2,2)
+plt.subplot(2,2,3)
+plt.subplot(2,2,4)
+//
+# plt.plot()
+plt.show()
